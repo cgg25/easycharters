835049f3d9c952b9ca7f40daaea2f8cf3a1f9fa2
cgg25 <carlosgarciagelabert1990@gmail.com>
2017-04-09 23:41:37 +0200

    modal,menu
    diseñado modal para login y sing up. Tambien se ha diseñado los menús
    para los distintos usuarios.

--- funciones/menu.php
+++ funciones/menu.php
@@ -1,35 +1,150 @@
 <?php
-	
-	if (empty($_SESSION['usuario'])) {?>
-		
-	
-	<nav class="navbar navbar-fixed-top  bg-primary">
-			<div class="container-fluid">
-		    	<div class="navbar-header">
-		    		<button type="button" class="navbar-toggle colorMenu" data-toggle="collapse" data-target="#myNavbar">
-				        <span class="icon-bar"></span>
-				        <span class="icon-bar"></span>
-				        <span class="icon-bar"></span>                        
-				    </button>
-			      	<a class="navbar-brand" href="#">
-				      	<div>
+	function Pintar_menu() {
+		if (empty($_SESSION['usuario'])) { ?>
+			<nav class="navbar">
+				<div class="container-fluid">
+			    	<div class="navbar-header">
+			    		<button type="button" class="navbar-toggle colorMenu" data-toggle="collapse" data-target="#myNavbar">
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>                        
+					    </button>
+				      	<a class="navbar-brand" href="#">
+				      	<div id="logo">
 				      		<img alt="Brand" src="logo.png">
 				       	 	<span class="colorLetraLogo">EasyCharter</span>
 				      	</div>
-			        </a>
-			    </div>
-			    <div class="collapse navbar-collapse" id="myNavbar">
-			      <ul class="nav navbar-nav">
-			        <li class="active"><a href="#">Home</a></li>
-			       	<li><a href="#">Reservas</a></li>
-			       	<li><a href="#">Como funciona</a></li>
-			      	<li><a href="#">Contacto</a></li>
-			      </ul>
-			      <ul class="nav navbar-nav navbar-right">
-			        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        			<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-			      </ul>
-			    </div>
-			</div>
-		</nav>
-<?php 	} ?>
+				        
+				      </a>
+				    </div>
+				    <div class="collapse navbar-collapse" id="myNavbar">
+				     
+				      <ul class="nav navbar-nav navbar-right">
+				        <li><a data-toggle="modal" href="#" id="singupMod"><span class="glyphicon glyphicon-user"></span> Regístrate</a></li>
+		    			<li><a data-toggle="modal" href="#" id="loginMod"><span class="glyphicon glyphicon-log-in"></span> Iniciar sesión</a></li>
+				      </ul>
+				    </div>
+				</div>
+			</nav>
+	<?php 	}else if ($_SESSION['usuario']->getKarma()==1) { ?>
+		
+			<nav class="navbar ">
+				<div class="container-fluid">
+			    	<div class="navbar-header">
+			    		<button type="button" class="navbar-toggle colorMenu" data-toggle="collapse" data-target="#myNavbar2">
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>                        
+					    </button>
+				      	<a class="navbar-brand" href="#">
+				      	<div id="logo">
+				      		<img alt="Brand" src="logo.png">
+				       	 	<span class="colorLetraLogo">EasyCharter</span>
+				      	</div>
+				        
+				      </a>
+				    </div>
+				    <div class="collapse navbar-collapse" id="myNavbar2">
+				     
+				      <ul class="nav navbar-nav navbar-right">
+				        <li class="dropdown">
+					        <a id="rueda" class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-cog fa-2x" aria-hidden="true"></i>
+					        </a>
+					        <ul class="dropdown-menu">
+					          <li><a href="#">Mis reservas</a></li>
+					          <li><a href="#">Guias</a></li>
+					          <li><a href="#">Mi cuenta</a></li>
+				           	  <li><a href="#">¿Tienes Barcos?</a></li>
+				           	  <li><a href="#">Aviso Legal</a></li>
+				              <li class="divider"></li>
+				              <li><a href="#">Log out</a></li>
+					        </ul>
+					      </li>
+				      </ul>
+				    </div>
+				</div>
+			</nav>
+
+	<?php }else if ($_SESSION['usuario']->getKarma()==2) { ?>
+		
+			<nav class="navbar ">
+				<div class="container-fluid">
+			    	<div class="navbar-header">
+			    		<button type="button" class="navbar-toggle colorMenu" data-toggle="collapse" data-target="#myNavbar2">
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>                        
+					    </button>
+				      	<a class="navbar-brand" href="#">
+				      	<div id="logo">
+				      		<img alt="Brand" src="logo.png">
+				       	 	<span class="colorLetraLogo">EasyCharter</span>
+				      	</div>
+				        
+				      </a>
+				    </div>
+				    <div class="collapse navbar-collapse" id="myNavbar2">
+				     
+				      <ul class="nav navbar-nav navbar-right">
+				        <li class="dropdown">
+					        <a id="rueda" class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-cog fa-2x" aria-hidden="true"></i>
+					        </a>
+					        <ul class="dropdown-menu">
+					          <li><a href="#">Mis reservas</a></li>
+					          <li><a href="#">Gestionar Barco</a></li>
+					          <li><a href="#">Mi cuenta</a></li>
+				           	  <li><a href="#">Guias</a></li>
+				           	  <li><a href="#">Aviso Legal</a></li>
+				              <li class="divider"></li>
+				              <li><a href="#">Log out</a></li>
+					        </ul>
+					      </li>
+				      </ul>
+				    </div>
+				</div>
+			</nav>
+
+
+
+
+	<?php	}elseif ($_SESSION['usuario']->getKarma()==3) { ?>
+			
+			<nav class="navbar ">
+				<div class="container-fluid">
+			    	<div class="navbar-header">
+			    		<button type="button" class="navbar-toggle colorMenu" data-toggle="collapse" data-target="#myNavbar2">
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>                        
+					    </button>
+				      	<a class="navbar-brand" href="#">
+				      	<div id="logo">
+				      		<img alt="Brand" src="logo.png">
+				       	 	<span class="colorLetraLogo">EasyCharter</span>
+				      	</div>
+				        
+				      </a>
+				    </div>
+				    <div class="collapse navbar-collapse" id="myNavbar2">
+				     
+				      <ul class="nav navbar-nav navbar-right">
+				        <li class="dropdown">
+					        <a id="rueda" class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-cog fa-2x" aria-hidden="true"></i>
+					        </a>
+					        <ul class="dropdown-menu">
+					          <li><a href="#">Gestionar web</a></li>
+					          <li><a href="#">Gestionar Barcos</a></li>
+					          <li><a href="#">Gestionar Perfiles</a></li>
+				           	  <li><a href="#">Historial Reservas</a></li>
+				           	  <li><a href="#">Gráfica Beneficios</a></li>
+				              <li class="divider"></li>
+				              <li><a href="#">Log out</a></li>
+					        </ul>
+					      </li>
+				      </ul>
+				    </div>
+				</div>
+			</nav>
+	<?php	} 
+	}
+ ?>
